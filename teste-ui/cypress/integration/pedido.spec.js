af351e9b9536caab6a24ad08fc36ba1d5232cd96
Felipe Dantas <feliperbdantas@outlook.com>
2022-03-05 18:02:44 -0300

    Change Documentation and Test UI

--- teste-ui/cypress/integration/pedido.spec.js
+++ teste-ui/cypress/integration/pedido.spec.js
@@ -1,27 +1,38 @@
 /// <reference types="cypress" />
 
-import pedidoPageObject from '../support/page_objects/pedido.page';
+import pedidoPage from '../support/page_objects/pedido.page';
 
 const pedido = require('../fixtures/pedido.json');
+const produto = require('../fixtures/produto.json');
 
-context('Adicionar itens ao carrinho', () => {
-    beforeEach(() => {
+describe('Adicionar itens ao carrinho', () => {
+    before(() => {
         cy.visit('minha-conta');
         cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha);
         });
     });
 
+    context('Dado que eu acesse a página de um produto', () => {
+        before(() => {
+            pedidoPage.paginaProduto(produto.id);
+        });
+
+        context(`Quando eu adicionar ${pedido.filter(item => item.processar).length} itens diferentes de um produto ao carrinho`, () => {
+            pedido.filter(item => item.processar)
+                .forEach(item => {
+                    before(() => {
+                        pedidoPage.pedido(item.tamanho, item.cor);
+                    });
+                });
+
+            it('Então deve retornar uma mensagem de sucesso', () => {
+                cy.get('.woocommerce-message').should('contain', `“${produto.titulo}” foi adicionado no seu carrinho.`);
+            });
+        });
+    });
+
     afterEach(() => {
         cy.screenshot();
     });
-
-    it('Deve adicionar 3 itens de produtos diferentes ao carrinho com sucesso', () => {
-        pedido.forEach(item => {
-            if (item.processar) {
-                pedidoPageObject.pedido(item.tamanho, item.cor);
-                cy.get('.woocommerce-message').should('contain', '“Abominable Hoodie” foi adicionado no seu carrinho.');
-            }
-        });
-    });
-})
+});
--- teste-ui/cypress/support/page_objects/pedido.page.js
+++ teste-ui/cypress/support/page_objects/pedido.page.js
@@ -1,7 +1,10 @@
 class Pedido {
-    pedido(size, color) {
-        cy.get('#primary-menu > .menu-item-629 > a').click();
+    paginaProduto(id) {
+        cy.get(`#primary-menu > .menu-item-${id} > a`).click();
         cy.get('.post-2559 > .product-block > .block-inner > .image > .product-image > .image-hover').click();
+    }
+
+    pedido(size, color) {
         cy.get('.button-variable-item-' + size).click();
         cy.get('.button-variable-item-' + color).click();
         cy.get('.single_add_to_cart_button').click();
